5cd327563750df4adfe995fdd3d026f52e328301
Tenaka <86342641+Tenaka@users.noreply.github.com>
2022-09-18 18:18:42 +0100

    Update MergeGPOs.1.0.ps1

--- MergeGPOs.1.0.ps1
+++ MergeGPOs.1.0.ps1
@@ -1,4 +1,4 @@
-﻿#Merge GPOs - copy this script to the root of a directory with Domain GPO's that require merging. 
+#Merge GPOs - copy this script to the root of a directory with Domain GPO's that require merging. 
 
 #Confirm for elevated admin
 if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
@@ -27,7 +27,19 @@ else
         $fileExt = $file.Extension
         $fullname = $file.FullName
 
-        if ($fileExt -eq ".csv"){& $lgpoExe /a $fullname}
+        if ($fileExt -eq ".csv")
+        {
+            <#
+            This is a little sub-optimal - The commands that should allow overlaying audit.csv's dont work so its a last apply wins. 
+            Use PolicyAnalyzer.exe to compare GPO's and manually create a RSoP for Audit settings.
+            
+            & $lgpoExe /a $fullname
+            & auditpol.exe /restore /file:$fullname
+            
+            #>
+            New-Item -path "C:\Windows\System32\GroupPolicy\Machine\Microsoft\Windows NT\" -Name Audit -ItemType Directory -Force
+            Copy -Path $fullname -Destination "C:\Windows\System32\GroupPolicy\Machine\Microsoft\Windows NT\Audit\" -Force
+        }
 
         if ($fileExt -eq ".pol"){& $lgpoExe /m $fullname}
 
