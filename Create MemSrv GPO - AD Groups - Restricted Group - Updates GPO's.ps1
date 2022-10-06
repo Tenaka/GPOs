32a8be22744b21cd2f995003169ad9675c920b93
Tenaka <86342641+Tenaka@users.noreply.github.com>
2022-10-06 20:15:17 +0100

    Update Create MemSrv GPO - AD Groups - Restricted Group - Updates GPO's.ps1

--- Create MemSrv GPO - AD Groups - Restricted Group - Updates GPO's.ps1
+++ Create MemSrv GPO - AD Groups - Restricted Group - Updates GPO's.ps1
@@ -1,8 +1,7 @@
-﻿
 
 
 #Root of all Member Server Resources
-$resRoot = "Test Resources"
+$resRoot = "Testing Resources"
 
 #Member Server OU's Name
 $memSrvRoot = "Member Servers"
@@ -13,45 +12,43 @@ $ResGroupRoot = "Restricted Groups"
 #List of Server or Applications Services
 $srvService = "Exchange","SCCM","SCOM","VMM","File","SharePoint","DHCP"
 
-$getMSRoot = Get-ADOrganizationalUnit -filter * | where {$_.DistinguishedName -like "*$MemSrvRoot*"} | Select-Object -Skip 1
+$resourceOU = "OU=$($resRoot),$($rootDSE)"
+$memSrvOU = "OU=$($memSrvRoot),OU=$($resRoot),$($rootDSE)"
+$ResGroupOU = "OU=$($ResGroupRoot),OU=$($resRoot),$($rootDSE)" 
+
+$getMSRoot = Get-ADOrganizationalUnit -filter * | where {$_.DistinguishedName -eq $resourceOU } #| Select-Object -Skip 1
+
 
 if ($getMSRoot -eq $null)
 {
     $rootDSE = (Get-ADRootDSE).rootDomainNamingContext
 
-    New-ADOrganizationalUnit -Name $resRoot
-
-    $resourceOU = "OU=" + "$resRoot" + "," + "$rootDSE"
-    $memSrvOU = "OU=" + "$memSrvRoot" + ",OU=" + "$resRoot" + "," + "$rootDSE"
-
-    New-ADOrganizationalUnit -name $memSrvRoot -Path $resourceOU
-    New-ADOrganizationalUnit -name $ResGroupRoot -Path $resourceOU
+    New-ADOrganizationalUnit -Name $resRoot -ProtectedFromAccidentalDeletion $false
+    New-ADOrganizationalUnit -name $memSrvRoot -Path $resourceOU -ProtectedFromAccidentalDeletion $false
+    New-ADOrganizationalUnit -name $ResGroupRoot -Path $resourceOU -ProtectedFromAccidentalDeletion $false
 
         Foreach ($svc in $srvService )
         {
-            New-ADOrganizationalUnit -name $svc -Path $memSrvOU
+           New-ADOrganizationalUnit -name $svc -Path $memSrvOU -ProtectedFromAccidentalDeletion $false
         }
 }
 
-$getOUMS = Get-ADOrganizationalUnit -Filter * | where {$_.DistinguishedName -like "*$resourceOU*" -and $_.Name -ne "$resRoot" -and $_.Name -ne "$ResGroupRoot"   -and $_.Name -eq "$memSrvRoot"} 
-$getOU = Get-ADOrganizationalUnit -Filter * | where {$_.DistinguishedName -like "*$resourceOU*" -and $_.Name -ne "$resRoot" -and $_.Name -ne "$ResGroupRoot"}
-
-#OU to create Restricted Groups
-$getOURG = ((Get-ADOrganizationalUnit -Filter * | where {$_.DistinguishedName -like "*$resourceOU*" -and $_.Name -ne "$resRoot" -and $_.Name -eq "$ResGroupRoot"}).DistinguishedName) #| Select-Object -Skip 1
+$getOUMS = Get-ADOrganizationalUnit -Filter * | where {$_.DistinguishedName -eq $memSrvOU} 
+$getOU = Get-ADOrganizationalUnit -Filter * | where {$_.DistinguishedName -like "*$memSrvOU*" -and $_.DistinguishedName -ne $memSrvOU}
 
 #One Group to Rule them all - Top OU Level Admin Group that has Admin rights on all Sub-OU Servers
 #Root Admin Group Name
-$rgRtAdminGp = "RG_" + $MemSrvRoot + "_Admin"
+$rgRtAdminGp = "RG_$($MemSrvRoot)_Admin"
 #Root RDP Group Name
-$rgRtRDPGp = "RG_" + $MemSrvRoot + "_RDP"
+$rgRtRDPGp = "RG_$($MemSrvRoot)_RDP"
 
 #Admin Group Description
 $rgRtAdminDescrip = "Members of this group have Administrator privileges on all Servers in sub-Ou's"
 #RDP Group Description
 $rgRtRDPDescrip = "Members of this group have Remote Desktop privileges on all Servers in sub-Ou's"
 
-New-ADGroup -Name $rgRtAdminGp –groupscope Global -Path $getOURG -Description $rgRtAdminDescrip
-New-ADGroup -Name $rgRtRDPGp –groupscope Global -Path $getOURG -Description $rgRtRDPDescrip
+New-ADGroup -Name $rgRtAdminGp –groupscope Global -Path $ResGroupOU -Description $rgRtAdminDescrip
+New-ADGroup -Name $rgRtRDPGp –groupscope Global -Path $ResGroupOU -Description $rgRtRDPDescrip
 
 #Get New Group Name and SID
 $getRtRGAdmin = Get-ADGroup $rgRtAdminGp
@@ -61,7 +58,7 @@ $getRtRGAdminSid = $getRtRGAdmin.SID.Value
 $getRtRGRDPSid = $getRtRGRDP.SID.Value
 
 #GPO Name
-$GPOName = "GPO_" + $MemSrvRoot + "_RestrictedGroup"
+$GPOName = "GPO_$($MemSrvRoot)_RestrictedGroup"
 
 #New GPO based on the service and linked to OU
 New-GPO -Name $GPOName | New-GPLink -Target $getOUMS.DistinguishedName
@@ -69,24 +66,26 @@ New-GPO -Name $GPOName | New-GPLink -Target $getOUMS.DistinguishedName
 $getGpoId = (Get-GPO $GPOName).id
 $getGPOPath = (Get-GPO $GPOName).path
 
-$sysvol = "C:\Windows\SYSVOL\domain\Policies\" + "{" + $getGpoId + "}" + "\Machine\Microsoft\Windows NT\SecEdit"
+Set-GPPermission -Guid $getGpoId -PermissionLevel GpoEditDeleteModifySecurity -TargetType Group -TargetName $rgAdminGp
+
+$sysvol = "C:\Windows\SYSVOL\domain\Policies\{$($getGpoId)}\Machine\Microsoft\Windows NT\SecEdit"
 
 New-Item -Path $sysvol -ItemType Directory -Force
 New-Item -Path $sysvol -Name GptTmpl.inf -ItemType File -Force
 
-$gptFile = $sysvol + "\GptTmpl.inf"
+$gptFile = "$($sysvol)\GptTmpl.inf"
 
 #S-1-5-32-544 = Administrator Group
 #S-1-5-32-555 = Remote Desktop Group
 #SeRemoteInteractiveLogonRight = Allow log on through Remote Desktop Services
 
 #Admin Group Sids for Restricted Groups
-$addConAdmin = "*S-1-5-32-544__Members = " + "*" + $getRtRGAdminSid
+$addConAdmin = "*S-1-5-32-544__Members = *$($getRtRGAdminSid)"
 #RDP Group Sids for Restricted Groups
-$addConRDP = "*S-1-5-32-555__Members = "+ "*" + $getRtRGRDPSid 
+$addConRDP = "*S-1-5-32-555__Members = *$($getRtRGRDPSid)" 
 
 #User Rights Assignments
-$addConURARemote = "SeRemoteInteractiveLogonRight = " + "*" + $getRtRGAdminSid + ",*" + $getRtRGRDPSid 
+$addConURARemote = "SeRemoteInteractiveLogonRight = *$($getRtRGAdminSid),*$($getRtRGRDPSid)" 
 
 #Update GmpTmpl.inf with URA and Restricted Groups
 Add-Content -Path $gptFile -Value '[Unicode]'
@@ -102,29 +101,28 @@ Add-Content -Path $gptFile -Value $addConRDP
 Add-Content -Path $gptFile -Value '[Privilege Rights]'
 Add-Content -Path $gptFile -Value $addConURARemote    
 
+#Set GPMC Machine Extensions so Manual Intervention is both displayed in GPO Management and applies to target 
 Set-ADObject -Identity $getGPOPath -Replace @{gPCMachineExtensionNames="[{827D319E-6EAC-11D2-A4EA-00C04F79F83A}{803E14A0-B4FB-11D0-A0D0-00A0C90F574B}]"}
 
-Foreach ($ouItem in $getOU)
-{
-    $ouName = $ouItem.Name
-    $ouDN = $ouItem.DistinguishedName
-
-    $Mem_SrvRoot = $MemSrvRoot.Replace(" ","_")
-
-    if ($ouName -ne "$memSrvRoot")
+    Foreach ($ouItem in $getOU)
     {
+        $ouName = $ouItem.Name
+        $ouDN = $ouItem.DistinguishedName
+
+        $Mem_SrvRoot = $MemSrvRoot.Replace(" ","_")
+    
         #Admin Group Name
-        $rgAdminGp = "RG_" +  $Mem_SrvRoot+ "_"+ "$ouName" + "_Admin"
+        $rgAdminGp = "RG_$($Mem_SrvRoot)_$($ouName)_Admin"
         #RDP Group Name
-        $rgRDPGp = "RG_" +  $Mem_SrvRoot + "_"+ "$ouName" + "_RDP"
+        $rgRDPGp = "RG_$($Mem_SrvRoot)_$($ouName)_RDP"
 
         #Admin Group Description
         $rgAdminDescrip = "Members of this group have Administrator privileges on the $ouName Servers"
         #RDP Group Description
         $rgRDPDescrip = "Members of this group have Remote Desktop privileges on the $ouName Servers"
 
-        New-ADGroup -Name $rgAdminGp –groupscope Global -Path $getOURG -Description $rgAdminDescrip
-        New-ADGroup -Name $rgRDPGp –groupscope Global -Path $getOURG -Description $rgRDPDescrip
+        New-ADGroup -Name $rgAdminGp –groupscope Global -Path $ResGroupOU -Description $rgAdminDescrip
+        New-ADGroup -Name $rgRDPGp –groupscope Global -Path $ResGroupOU -Description $rgRDPDescrip
 
         #Get New Group Name and SID
         $getRGAdmin = Get-ADGroup $rgAdminGp
@@ -134,7 +132,7 @@ Foreach ($ouItem in $getOU)
         $getRGRDPSid = $getRGRDP.SID.Value
 
         #GPO Name
-        $GPOName = "GPO_" +  $Mem_SrvRoot + "_"+ "$ouName" + "_RestrictedGroup"
+        $GPOName = "GPO_$($Mem_SrvRoot)_$($ouName)_RestrictedGroup"
 
         #New GPO based on the service and linked to OU
         New-GPO -Name $GPOName | New-GPLink -Target $ouDN 
@@ -142,7 +140,9 @@ Foreach ($ouItem in $getOU)
         $getGpoId = (Get-GPO $GPOName).id 
         $getGPOPath = (Get-GPO $GPOName).path
 
-        $sysvol = "C:\Windows\SYSVOL\domain\Policies\" + "{" + $getGpoId + "}" + "\Machine\Microsoft\Windows NT\SecEdit"
+        Set-GPPermission -Guid $getGpoId -PermissionLevel GpoEditDeleteModifySecurity -TargetType Group -TargetName $rgAdminGp
+
+        $sysvol = "C:\Windows\SYSVOL\domain\Policies\{$($getGpoId)}\Machine\Microsoft\Windows NT\SecEdit"
 
         New-Item -Path $sysvol -ItemType Directory -Force
         New-Item -Path $sysvol -Name GptTmpl.inf -ItemType File -Force
@@ -154,12 +154,12 @@ Foreach ($ouItem in $getOU)
         #SeRemoteInteractiveLogonRight = Allow log on through Remote Desktop Services
 
         #Admin Group Sids for Restricted Groups
-        $addConAdmin = "*S-1-5-32-544__Members = " + "*" + $getRtRGAdminSid + "," + "*" + $getRGAdminSid
+        $addConAdmin = "*S-1-5-32-544__Members = *$($getRtRGAdminSid),*$($getRGAdminSid)"
         #RDP Group Sids for Restricted Groups
-        $addConRDP = "*S-1-5-32-555__Members = "+ "*" + $getRtRGRDPSid + "," + "*" + $getRGRDPSid
+        $addConRDP = "*S-1-5-32-555__Members = *$($getRtRGRDPSid),*$($getRGRDPSid)"
 
         #User Rights Assignments
-        $addConURARemote = "SeRemoteInteractiveLogonRight = " + "*" + $getRtRGAdminSid + "," + "*" + $getRGAdminSid + "," + "*" + $getRtRGRDPSid + "," + "*" + $getRGRDPSid
+        $addConURARemote = "SeRemoteInteractiveLogonRight = *$($getRtRGAdminSid),*$($getRGAdminSid),*$($getRtRGRDPSid),*$($getRGRDPSid)"
 
         #Update GmpTmpl.inf with URA and Restricted Groups
         Add-Content -Path $gptFile -Value '[Unicode]'
@@ -177,9 +177,6 @@ Foreach ($ouItem in $getOU)
 
         #Set GPMC Machine Extensions so Manual Intervention is both displayed in GPO Management and applies to target 
         Set-ADObject -Identity $getGPOPath -Replace @{gPCMachineExtensionNames="[{827D319E-6EAC-11D2-A4EA-00C04F79F83A}{803E14A0-B4FB-11D0-A0D0-00A0C90F574B}]"}
-
+       
     }
-
-}
-
-
+    
